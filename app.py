39321daded368687d6f09e627738f7bb4d175b7f
oooh57666-design <oooh57666@gmail.com>
2025-12-04 11:02:16 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Trivia Game", layout="wide")
+
+st.title("Trivia Game")
+st.write("Deployment successful! The real game code will be added soon.")
